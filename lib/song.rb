51710c0bf03817994c23a0b6b81d5410e588c507
christian williams <christian.williamsj@gmail.com>
2019-09-17 05:46:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,6 +25,18 @@ class Song
   def self.find_by_name(string_name_of_the_song) 
     self.all.detect {|i| i.name == string_name_of_the_song}
   end
+  
+    def self.find_or_create_by_name(find_this_song)
+    did_i_find_it = self.all.detect {|x| x.name == find_this_song}
+    if did_i_find_it == nil
+      s = self.new 
+      s.name = find_this_song
+      s.save 
+      s
+    else
+      did_i_find_it
+    end
+  end 
 
   def self.all
     @@all
