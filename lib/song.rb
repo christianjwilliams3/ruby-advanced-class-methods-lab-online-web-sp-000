07c98cbbb7fe2999aea16438bdda73800f89c1d9
christian williams <christian.williamsj@gmail.com>
2019-09-17 05:47:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,13 +37,41 @@ class Song
       did_i_find_it
     end
   end 
-
+  def self.alphabetical
+    self.all.sort_by { |x| x.name}
+  end 
+  
+  def self.new_from_filename(mp3_formatted_file)
+    # Build a class constructor that accepts a filename in the 
+    # format of " - .mp3", for example "Taylor Swift - Blank Space.mp3".
+    
+    # Given Song.new_from_filename("Taylor Swift - Blank Space.mp3"), 
+    # the constructor should return a new Song instance with the song 
+    # name set to Blank Space and the artist_name set to Taylor Swift.
+    c = self.new 
+    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    c
+  end 
+  
+  def self.create_from_filename(mp3_formatted_file)
+    c = self.new 
+    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    c.save
+    c
+  end 
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
+  
+  def self.destroy_all
+    self.all.clear
+  end
+ 
 end
